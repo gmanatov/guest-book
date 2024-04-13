f3116c0207e39b7d771a2951ea6710d5f7ca3145
gmanatov <jfexpr@gmail.com>
2024-04-13 14:14:20 -0500

    Introduced some mongoose code into models/entry.js

--- models/entry.js
+++ models/entry.js
@@ -1,6 +1,27 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+	
+const entrySchema = new Schema({
+  id: Number,
+  date: String,
+  time: String,
+  author: String,
+  content: String
+}, {
+    timestamps: true
+  });
+	
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Entry', entrySchema);
+
+
+
+
+
 const entries = [
     {id: 124624, date: '09/28/1975', time: '03:55:06 PM', author: 'Jack Torrance', content: 'We just moved in, and what a wonderful building building this is...'},
-    {id: 125223, date: '10/13/1975', time: '01:20:20 AM', author: 'Jack Torrence', content: 'This walls are crushing me...'},
+    {id: 125223, date: '10/13/1975', time: '01:20:20 AM', author: 'Jack Torrence', content: 'These walls are crushing me...'},
     {id: 127904, date: '11/05/1975', time: '06:00:00 AM', author: 'Jack Torrens', content: 'I have to keep writing...'},
     {id: 139608, date: '01/06/1976', time: '04:02:00 AM', author: 'JACK THE MURDER', content: 'Where is this little NASTY BOY...'},
   ];
