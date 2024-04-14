abe245cf8587cd5c3520311b2a19086c8cecbe1f
gmanatov <jfexpr@gmail.com>
2024-04-13 19:44:50 -0500

    Dealt with a weird error that node would give while adding document to database...onto CRUD using database...

--- models/entry.js
+++ models/entry.js
@@ -1,74 +1,93 @@
-const mongoose = require('mongoose');
-// optional shortcut to the mongoose.Schema class
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 	
-const entrySchema = new Schema({
-  id: Number,
-  date: String,
-  time: String,
-  author: String,
-  content: String,
-}, {
-    timestamps: true
-  });
+// const entrySchema = new Schema({
+//   id: Number,
+//   date: String,
+//   time: String,
+//   author: String,
+//   content: String,
+// }, {
+//     timestamps: true
+//   });
 	
-// Compile the schema into a model and export it
-module.exports = mongoose.model('Entry', entrySchema);
+// // Compile the schema into a model and export it
+// console.log("Loading the entry model.");
+// module.exports = mongoose.model('Entry', entrySchema);
 
 
+////////////////////////////////////////
+
+// const mongoose = require('mongoose');
+
+// const entrySchema = new mongoose.Schema({
+//   id: Number,
+//   date: String,
+//   time: String,
+//   author: String,
+//   content: String
+// }, {
+//   timestamps: true
+// });
+
+// const Entry = mongoose.model('Entry', entrySchema);
+
+// module.exports = Entry;
 
 
+////////////////////////LEGACY NON MONGODB MODEL///////////////
 
-// const entries = [
-//     {id: 124624, date: '09/28/1975', time: '03:55:06 PM', author: 'Jack Torrance', content: 'We just moved in, and what a wonderful building this is...'},
-//     {id: 125223, date: '10/13/1975', time: '01:20:20 AM', author: 'Jack Torrence', content: 'These walls are crushing me...'},
-//     {id: 127904, date: '11/05/1975', time: '06:00:00 AM', author: 'Jack Torrens', content: 'I have to keep writing...'},
-//   ];
 
-//   module.exports = {
-//     getAll,
-//     deleteOne,
-//     getOne,
-//     create,
-//     update
-//   };
+const entries = [
+    {id: 124624, date: '09/28/1975', time: '03:55:06 PM', author: 'Jack Torrance', content: 'We just moved in, and what a wonderful building this is...'},
+    {id: 125223, date: '10/13/1975', time: '01:20:20 AM', author: 'Jack Torrence', content: 'These walls are crushing me...'},
+    {id: 127904, date: '11/05/1975', time: '06:00:00 AM', author: 'Jack Torrens', content: 'I have to keep writing...'},
+  ];
+
+  module.exports = {
+    getAll,
+    deleteOne,
+    getOne,
+    create,
+    update
+  };
   	
-//   function getAll() {
-//     return entries;
-//   }
+  function getAll() {
+    return entries;
+  }
 
-//   function deleteOne(id) {
-//     id = parseInt(id)
-//     // Find the index for the entry
-//     const idx = entries.findIndex(entry => entry.id === id)
-//     entries.splice(idx, 1)
-//   }
+  function deleteOne(id) {
+    id = parseInt(id)
+    // Find the index for the entry
+    const idx = entries.findIndex(entry => entry.id === id)
+    entries.splice(idx, 1)
+  }
 
-//   function getOne(id) {
-//     id = parseInt(id)
-//     return entries.find(entry => entry.id === id)
-//   }
+  function getOne(id) {
+    id = parseInt(id)
+    return entries.find(entry => entry.id === id)
+  }
 
-//   function create(entry) {
-//     let now = new Date();
-//     // "Randomizing ID"
-//     entry.id = Date.now() % 1000000
-//     let month = now.getMonth() + 1
-//     if (month < 10) {month = '0' + month}
-//     let day = now.getDate()
-//     if (day < 10) {day = '0' + day}
-//     let year = now.getFullYear()
-//     entry.date = `${month}/${day}/${year}`
-//     let time = new Date().toLocaleTimeString()
-//     if (time[1] == ':') {time = '0' + time}
-//     entry.time = time
-//     if (entry.author == '') {entry.author = '<blank>'}
-//     entries.push(entry)
-//   }
+  function create(entry) {
+    let now = new Date();
+    // "Randomizing ID"
+    entry.id = Date.now() % 1000000
+    let month = now.getMonth() + 1
+    if (month < 10) {month = '0' + month}
+    let day = now.getDate()
+    if (day < 10) {day = '0' + day}
+    let year = now.getFullYear()
+    entry.date = `${month}/${day}/${year}`
+    let time = new Date().toLocaleTimeString()
+    if (time[1] == ':') {time = '0' + time}
+    entry.time = time
+    if (entry.author == '') {entry.author = '<blank>'}
+    entries.push(entry)
+  }
 
-//   function update(id, updatedEntry){
-//     id = parseInt(id)
-//     const entry = entries.find(entry => entry.id === id)
-//     //entry.content = updatedEntry.content
-//     Object.assign(entry, updatedEntry)
-//   }
+  function update(id, updatedEntry){
+    id = parseInt(id)
+    const entry = entries.find(entry => entry.id === id)
+    //entry.content = updatedEntry.content
+    Object.assign(entry, updatedEntry)
+  }
